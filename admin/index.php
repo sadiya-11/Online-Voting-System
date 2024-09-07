6298f5a8c3705179645591093408615ce445b627
sadiya-11 <142381362+sadiya-11@users.noreply.github.com>
2024-09-07 12:26:44 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -2,23 +2,19 @@
 require_once("incc/header.php");
 require_once("incc/nav.php");
 
-if(isset($_GET['homepage']))
-{
-require_once("incc/homepage.php");
-}
-else if(isset($_GET['addElectionPage']))
-{
-require_once("incc/add_election.php");
-}
-else  if(isset($_GET['addCandidatePage']))
-{
+if (isset($_GET['homepage'])) {
+    require_once("incc/homepage.php");
+} 
+else if (isset($_GET['addElectionPage'])) {
+    require_once("incc/add_elections.php");
+} 
+else if (isset($_GET['addCandidatePage'])) {
     require_once("incc/add_candidates.php");
+} 
+else if (isset($_GET['viewResult'])) {
+    require_once("incc/viewResults.php");
 }
-
 ?>
-
-
-
 
 <?php
 require_once("incc/footer.php");
