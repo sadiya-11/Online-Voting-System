81ce16ec9d090b79691725fc16b689fd35b972e4
sadiya-11 <142381362+sadiya-11@users.noreply.github.com>
2024-09-07 12:24:57 +0530

    Update footer.php

--- admin/incc/footer.php
+++ admin/incc/footer.php
@@ -1,16 +1,8 @@
 <div class="row bg-black text-white text-center">
-            
-            <div class="col-12 my-3">
-                <p>&copy; Copyright -All rights reserved <br/> by sadiya</p>
-            </div>
-        </div>
-   
-
-
-
-
-</div>
-
+    <div class="col-12 my-3">
+    <p>&copy; Copyright -All rights reserved <br/> by sadiya</p>
+    </div>
+ </div>
 <script src="../assets/js/jquery.min.js"></script>
 <script src="../assets/js/bootstrap.min.js"></script>
 </body>
